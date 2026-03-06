6e3a4199222f275c63b7cb440062305a8c9883f9
Kelvin <haminhhoang19@gmail.com>
2026-03-07 00:35:11 +1100

    Bug fix on deployment

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,6 @@
-const API_BASE = "http://localhost:8000";
+const API_BASE = window.location.hostname === "localhost"
+    ? "http://localhost:8000"
+    : "";
 
 // DOM Elements
 const studyForm = document.getElementById("studyForm");
