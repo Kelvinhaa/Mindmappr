cdb325b678dd2bf59870c91a3d49e3002de1e7ad
Kelvin <haminhhoang19@gmail.com>
2026-03-07 00:26:22 +1100

    Bug fix on deployment

--- backend/backends/main.py
+++ backend/backends/main.py
@@ -10,6 +10,7 @@ origins = [
     "http://127.0.0.1:3000",
     "http://127.0.0.1:5500",
     "http://localhost:5500",
+    "https://mindmappr-alpha.vercel.app",
 ]
 app.add_middleware(
     CORSMiddleware, 
